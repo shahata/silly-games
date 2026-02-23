5a6c8f9e1a7f9dbf642fd09407895444304ff8af
Shahar Talmi <shahar@wix.com>
2026-02-23 05:10:09 +0200

    base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/silly-games/",
   build: {
     minify: false,
     rollupOptions: {
