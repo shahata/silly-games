4af0ee71c9e0a045b742e61ddf78cf844f1d1348
Shahar Talmi <shahar@wix.com>
2026-03-08 14:45:52 +0200

    separated beer glass to different file

--- tapper/src/Beers.js
+++ tapper/src/Beers.js
@@ -1,84 +1,12 @@
 import Player from "./Player.js";
 import Customers from "./Customers.js";
-import LevelManager, {
-  ROW_LEFT_BOUNDS,
-  ROW_RIGHT_BOUNDS,
-  ROW_Y_POSITIONS,
-  SCORE_EMPTY_BEER,
-} from "./LevelManager.js";
+import LevelManager, { SCORE_EMPTY_BEER } from "./LevelManager.js";
 import SoundManager, { GRAB_MUG } from "./SoundManager.js";
 import ResourceManager from "./ResourceManager.js";
 import GameState, { STATE_PLAY } from "./GameState.js";
-
-const SPRITE_FULL_1 = 0;
-const SPRITE_FULL_2 = 1;
-const SPRITE_EMPTY_1 = 2;
-const SPRITE_FALLING = 3;
-const SPRITE_BROKEN = 4;
-
-const STEP_FULL = 4;
-const STEP_EMPTY = 1;
+import BeerGlass from "./BeerGlass.js";
 
 const SPRITE_WIDTH = 32;
-const SPRITE_HEIGHT = 32;
-
-class Glass {
-  #sprite = SPRITE_FULL_1;
-  #xPosition;
-  #isFull;
-  #fpsCount = 0;
-  #row;
-
-  constructor(row, xPosition, isFull) {
-    this.#row = row;
-    this.#xPosition = xPosition;
-    this.#isFull = isFull;
-  }
-
-  get xPosition() {
-    return this.#xPosition;
-  }
-
-  get isFull() {
-    return this.#isFull;
-  }
-
-  update() {
-    if (this.#isFull) {
-      if (this.#xPosition > ROW_LEFT_BOUNDS[this.#row]) {
-        this.#sprite = this.#fpsCount++ & 8 ? SPRITE_FULL_1 : SPRITE_FULL_2;
-        this.#xPosition -= STEP_FULL;
-      } else {
-        this.#sprite = SPRITE_BROKEN;
-        return true;
-      }
-    } else {
-      if (this.#xPosition < ROW_RIGHT_BOUNDS[this.#row] + SPRITE_WIDTH) {
-        this.#sprite = SPRITE_EMPTY_1;
-        this.#xPosition += STEP_EMPTY;
-      } else {
-        this.#sprite = SPRITE_FALLING;
-        return true;
-      }
-    }
-  }
-
-  draw(context, spriteImage) {
-    let yPosition = ROW_Y_POSITIONS[this.#row] + 8;
-    if (this.#sprite === SPRITE_FALLING) yPosition += SPRITE_HEIGHT;
-    context.drawImage(
-      spriteImage,
-      this.#sprite * SPRITE_WIDTH,
-      0,
-      SPRITE_WIDTH,
-      SPRITE_HEIGHT,
-      this.#xPosition,
-      yPosition,
-      SPRITE_WIDTH,
-      SPRITE_HEIGHT,
-    );
-  }
-}
 
 class Beers {
   #glasses;
@@ -89,7 +17,7 @@ class Beers {
   }
 
   add(row, xPosition, isFull) {
-    this.#glasses[row].push(new Glass(row, xPosition, isFull));
+    this.#glasses[row].push(new BeerGlass(row, xPosition, isFull));
   }
 
   #checkCollision(glass, row) {
